934c14151c4c506ac6b73cacb6f42ffb04183595
Michael McPherson <michael@mpmcpherson.com>
2022-09-08 15:15:08 -0400

    autocommit Thu 08 Sep 2022 03:15:08 PM EDT

--- going/begin.go
+++ going/begin.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"example.com/greetings"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println(Hello("Michael"))
-	message := greetings.Hello("Michael")
+	fmt.Println("Who are you?")
+	reader := bufio.NewReader(os.Stdin)
+	// ReadString will block until the delimiter is entered
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+
+	// remove the delimeter from the string
+	input = strings.TrimSuffix(input, "\n")
+	
+	
+	message := Hello(input)
+	
+	fmt.Println(message)
+}
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
--- going/go.mod
+++ going/go.mod
@@ -1,1 +1,5 @@
 module begin
+
+go 1.13
+
+require fyne.io/fyne/v2 v2.2.3 // indirect
