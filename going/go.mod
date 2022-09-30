959ca7048f47923f8ac99b6cf0d489bf48572916
Michael McPherson <michael@mpmcpherson.com>
2022-09-30 18:00:20 -0400

    autocommit Fri 30 Sep 2022 06:00:20 PM EDT

--- going/go.mod
+++ going/go.mod
@@ -2,4 +2,7 @@ module begin
 
 go 1.13
 
-require fyne.io/fyne/v2 v2.2.3 // indirect
+require (
+	fyne.io/fyne v1.4.3 // indirect
+	fyne.io/fyne/v2 v2.2.3
+)
--- going/uiTest.go
+++ going/uiTest.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	//"image/color"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
-
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	application := app.New()
+	newWindow := application.NewWindow("Box Layout")
+	
+    newWindow.SetMainMenu(menuSetup())
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+    //create shortcuts
+	arr := ShortCutSetup()    
+	
+	//instantiate shortcuts
+	for _, element := range arr {	
+		newWindow.Canvas().AddShortcut(element,func(shortcut fyne.Shortcut){
+				newWindow.Close()
+		})
+	}
 
-	w.ShowAndRun()
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter text...")
+
+
+	bottomCenter := container.New(layout.NewMaxLayout(), layout.NewSpacer(), input)
+
+	newWindow.SetContent(container.New(layout.NewVBoxLayout(), bottomCenter))
+
+	newWindow.Resize(fyne.NewSize(600,300))
+
+	newWindow.ShowAndRun()
+}
+
+func ShortCutSetup() []*desktop.CustomShortcut {
+	ctrlQ := &desktop.CustomShortcut{
+    	KeyName: fyne.KeyQ, 
+    	Modifier: fyne.KeyModifierControl,
+    }
+
+    return []*desktop.CustomShortcut{ctrlQ}
+}
+
+func menuSetup() *fyne.MainMenu {
+	menuItem1 := fyne.NewMenuItem("New", nil)
+    menuItem2 := fyne.NewMenuItem("Save", nil)
+    menuItem3 := fyne.NewMenuItem("edit", nil)
+    
+    newMenu := fyne.NewMenu("File", menuItem1, menuItem2, menuItem3)
+    
+    menu := fyne.NewMainMenu(newMenu) 
+
+    return menu
 }
